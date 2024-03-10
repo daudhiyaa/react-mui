0e32d77f508ff510291a93456cb2f7e9607d46f8
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-10 15:27:56 +0700

    feat: DateTimePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { MuiTypography } from "./components/MuiTypography";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import AdapterDateFns from "@mui/x-date-pickers/AdapterDateFns";
 
 function App() {
   return (
-    <div className="App">
-      <MuiTypography />
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <div className="App">
+        <MuiTypography />
+      </div>
+    </LocalizationProvider>
   );
 }
 
